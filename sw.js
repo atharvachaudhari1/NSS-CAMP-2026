03d65ffa99f484a3c43e2534768d68a5d7edcf40
atharvachaudhari1 <atharvachaudhari1@gmail.com>
2026-02-14 23:39:43 +0530

    Convert website to PWA: Add manifest, service worker, and installable Packing List

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'nss-camp-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './packing-list.html',
+    './styles.css',
+    './script.js',
+    './nss.jpg',
+    './nss_logo.svg',
+    './consent-form.pdf',
+    './rulebook.pdf',
+    './packing-list.pdf'
+];
+
+// Install Event - Cache Files
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Activate Event - Clean Up Old Caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Event - Serve Cached Files
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Return cached response if found, else fetch from network
+            return response || fetch(event.request);
+        })
+    );
+});
